a655cb9e1f2f8f2e2a2d4497ac5666636476623d
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-29 20:23:05 +0200

    upload image register done :D

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,125 +1,76 @@
+import { notification } from "antd";
+import {  useState } from "react";
+import { useDispatch} from "react-redux";
+import { register} from "../../features/auth/authSlice";
 
-import { Form, Input, Button, notification ,message, Upload,InputNumber} from 'antd';
-import { UserOutlined, MailOutlined, LockOutlined,  UploadOutlined } from '@ant-design/icons';
-import { useDispatch } from 'react-redux'
-import {register} from '../../features/auth/authSlice'
+const Register = () => {
+  const initialState = {
+    username: "",
+    age:"",
+    email: "",
+    password: "",
+    password2: "",
+    image:""
+  };
+  const [formData, setFormData] = useState(initialState);
+  const { username, age, email, password, password2,image } = formData;
 
-const Register=()=> {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
 
-  const onFinish = (values) => {
-    console.log('valores formulario',values)
-    dispatch(register(values))
-    return notification.success({
-      message: "Bienvenido!",
-      description: "Usuario creado con éxito!",
-    });
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.error("Failed:", errorInfo);
-  };
- 
-  const props = {
-    name: 'image',
-    action: 'http://localhost:8080/users/',
-   
-  
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-  
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-  };
-
-
-  return (
-   
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 29 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Nombre de usuario"
-            name="username"
-            rules={[{ required: true, message: "Introduce un nombre de usuario" }]}
-          >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} />
-          </Form.Item>
-
-          <Form.Item 
-            label="Edad"
-            name="age"
-            rules={[{ required: true, message: "Introduce tu edad" }]}
-          >
-            <InputNumber  min={16} />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Introduce tu correo" }]}
-          >
-            <Input prefix={<MailOutlined className="site-form-item-icon" />} />
-          </Form.Item>
-         
-          <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Introduce una contraseña',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password prefix={<LockOutlined className="site-form-item-icon" />}/>
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirma Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Confirma tu contraseña',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('Las contraseñas no coinciden!'));
-            },
-          }),
-        ]}
-      >
-        <Input.Password  prefix={<LockOutlined className="site-form-item-icon" />}/>
-      </Form.Item>
-      <Upload {...props}>
-    <Button icon={<UploadOutlined />}>Click to Upload</Button>
-  </Upload>
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Registrarse
-            </Button>
-          </Form.Item>
-        </Form>
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      notification.error({ message: "Las contraseñas no coindicen" });
+      
+    } else {
+        const formData = new FormData();
+        if (e.target.image.files[0]) formData.set('image', e.target.image.files[0]);
+        formData.set('username', e.target.username.value)
+        formData.set('age', e.target.age.value)
+        formData.set('email', e.target.email.value)
+        formData.set('password', e.target.password.value)
     
+      dispatch(register(formData));
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+        <label htmlFor="username">Nombre de usuario:</label> <br />
+      <input type="text" name="username" value={username} onChange={onChange} /> <br />
+      <label htmlFor="email">Correo: </label> <br />
+      <input type="email" name="email" value={email} onChange={onChange} /> <br />
+      <label htmlFor="age">Edad:</label> <br />
+      <input type="number" name="age" value={age} onChange={onChange} /> <br />
+<label htmlFor="password">Introduce tu contraseña:</label> <br />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={onChange}
+      /> <br />
+      <label htmlFor="password2">Introduce de nuevo tu contraseña:</label> <br />
+
+      <input
+        type="password"
+        name="password2"
+        value={password2}
+        onChange={onChange}
+      />
+      <input 
+       onChange={onChange}
+       type="file" value={image} name='image'/>
+      <button>Subir foto</button>
+      <input type="submit" />
+    </form>
   );
-}
-export default Register
+};
+
+export default Register;
