c1f6a79161039188bc5fd9bb62951c2135e45876
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-29 17:12:09 +0200

    created component register

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Register from "./components/Register/Register";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-    
+    <BrowserRouter>
+<Routes>
+<Route path="/register" element={<Register />} />
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,69 @@
+
+import { Form, Input, Button, notification , InputNumber} from 'antd';
+import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
+
+
+const Register=()=> {
+  const onFinish = (values) => {
+    console.log('valores formulario',values)
+    return notification.success({
+      message: "Bienvenido!",
+      description: "Usuario creado con éxito!",
+    });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.error("Failed:", errorInfo);
+  };
+  return (
+   
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 29 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Nombre de usuario"
+            name="username"
+            rules={[{ required: true, message: "Introduce un nombre de usuario" }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} />
+          </Form.Item>
+
+          <Form.Item
+            label="Edad"
+            name="age"
+            rules={[{ required: true, message: "Introduce tu edad" }]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Introduce tu correo" }]}
+          >
+            <Input prefix={<MailOutlined className="site-form-item-icon" />} />
+          </Form.Item>
+         
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Introduce una contraseña" }]}
+          >
+            <Input.Password prefix={<LockOutlined className="site-form-item-icon" />} />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Registrarse
+            </Button>
+          </Form.Item>
+        </Form>
+    
+  );
+}
+export default Register
