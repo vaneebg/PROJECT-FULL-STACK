df682713f546779030d0c04da06582956f0604f4
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-30 16:07:00 +0200

    montados componentes en main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Profile from './Profile/Profile'
+import Posts from './Posts/Posts'
+import './Main.scss'
 
 const Main = () => {
-  return (
-    <div>Main</div>
+  return (<div className='main'>
     
+    <Posts/>
+    <Profile/>
+    </div>
   )
 }
 
--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useDispatch } from "react-redux";
-import { logout } from "../../features/auth/authSlice";
+import { logout } from "../../../features/auth/authSlice";
+import { Link } from 'react-router-dom'
+import { PoweroffOutlined } from "@ant-design/icons";
+
 
 
 const Profile = () => {
@@ -9,15 +12,16 @@ const Profile = () => {
     const onLogout = (e) => {
       e.preventDefault();
       dispatch(logout());
+      };
 
-  return (<>
-    <div>Profile</div>
-    <Link to="/login" onClick={onLogout}><PoweroffOutlined /></Link>
+  return (
+    <div className='profile'>
+      <span>Perfil</span>
+    <Link to="/" onClick={onLogout}><PoweroffOutlined /></Link>
            
-          
-          </>
+    </div>
   )
 }
-}
+
 
 export default Profile
