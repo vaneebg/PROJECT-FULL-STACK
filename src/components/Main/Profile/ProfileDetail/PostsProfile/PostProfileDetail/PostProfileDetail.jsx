3bf298a41d2dfa93436db737e4b9f441506acef2
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-02 17:56:34 +0200

    puestos comments en postdetail

--- src/components/Main/Profile/ProfileDetail/PostsProfile/PostProfileDetail/PostProfileDetail.jsx
+++ src/components/Main/Profile/ProfileDetail/PostsProfile/PostProfileDetail/PostProfileDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPostById } from "../../../../../../features/posts/postsSlice";
+import Comments from "../../../../Posts/Post/Comments/Comments";
 import './PostProfileDetail.scss'
 
 const PostProfileDetail = () => {
@@ -12,6 +13,22 @@ const PostProfileDetail = () => {
       dispatch(getPostById(_id));
     }, []);
     console.log(post)
+    const comments=post.commentsId?.map((el,i)=>{return(
+      <div key={i}className="comments">
+          <div className="userC">
+  {el.userId.image ? <img className='imgUserC'src={"http://localhost:8080/images/users/" + el.userId.image} alt=''/> : null}
+  <span>{el.userId.username}</span>
+  <span>{el.createdAt}</span>
+  </div>
+  <div className="textC">
+        <span className='bold'>{el.title}&nbsp;</span>  
+        <span className='italic'>{el.body}</span> <br />
+        {el.image ? <img className='gifComment'src={"http://localhost:8080/images/comments/" + el.image} alt=''/> : null}
+        </div>
+        <div className="userComment">
+  </div>
+      </div>
+  )})
     return (
       <div className='postProfileDetail'>
     {post.image ? <img key={post._id} className='imageProfileDetail' src={"http://localhost:8080/images/posts/" + post.image} alt=''/> : null}
@@ -19,6 +36,7 @@ const PostProfileDetail = () => {
         <h2>{post.title}</h2>
         <p>{post.body}</p>
         <span>{post.createdAt}</span>
+        {comments}
       </div>
     );
 };
