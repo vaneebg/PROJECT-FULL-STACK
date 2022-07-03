c2a87d644c7604aa4e300a458a71ae93159acb4c
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-03 14:45:18 +0200

    añadido tooltip

--- src/components/Main/Profile/ProfileDetail/PostsProfile/PostsProfile.jsx
+++ src/components/Main/Profile/ProfileDetail/PostsProfile/PostsProfile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom'
+import { Tooltip } from 'antd';
 
 
 import './PostsProfile.scss'
@@ -13,10 +14,13 @@ const PostsProfile = () => {
     return <h1>Cargando posts..</h1>;
   }
       const post =user.user.postsId.map((el)=>{
-    return(
-        <><Link to={"/post/" + el._id}>
+    return(<>
+      <Tooltip title="Click para más info"color='purple' key='purple'>
+  <Link to={"/post/" + el._id}>
      {el.image ? <img key={el._id} className='postsProfile' src={"http://localhost:8080/images/posts/" + el.image} alt=''/> : null}
-        </Link>
+        </Link>  
+        </Tooltip>
+      
 
         </>
     )
