4d647a01f0af336a7c17cbeb4e8782895edf4c52
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-10 16:33:55 +0200

    cypress test login funcionando

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -7,21 +7,36 @@ describe('Probando ciclo de autenticación', () => {
 
   it("Probando register", function() {
     cy.get('.ant-tabs-nav-wrap > .ant-tabs-nav-list > .ant-tabs-tab > #rc-tabs-0-tab-1 > .contentHome').click()
-   cy.get('input[name="username"]').click().type('german')
+  //  cy.get('input[name="username"]').click().type('german')
+  //  cy.get('input[name="email"]').click().type('german@gmail.com')
+  //  cy.get('input[name="age"]').click().type('31')
+  //  cy.get('input[name="password"]').click().type('test')
+  //  cy.get('input[name="password2"]').click().type('test')
+  //  cy.get('input[type="file"]').attachFile('1.png');
+  //  cy.wait(3000)
+  //  cy.get('[type="submit"]').click()
+  //  cy.wait(1000)
+  //  cy.get('div > .ant-notification-notice > .ant-notification-notice-content > .ant-notification-notice-with-icon > .ant-notification-notice-description').should('have.text','Usuario registrado con éxito')  
+  });
+
+
+   it("Probando login", function() {
+  cy.get('.ant-tabs-nav-wrap > .ant-tabs-nav-list > .ant-tabs-tab > #rc-tabs-0-tab-2 > .contentHome').click()
    cy.get('input[name="email"]').click().type('german@gmail.com')
-   cy.get('input[name="age"]').click().type('31')
    cy.get('input[name="password"]').click().type('test')
-   cy.get('input[name="password2"]').click().type('test')
-   cy.get('input[type="file"]').attachFile('1.png');
-   cy.wait(3000)
    cy.get('[type="submit"]').click()
-   cy.wait(1000)
-
-   cy.get('div > .ant-notification-notice > .ant-notification-notice-content > .ant-notification-notice-with-icon > .ant-notification-notice-description').should('have.text','Usuario registrado con éxito')  
+   cy.get('div > .ant-notification-notice > .ant-notification-notice-content > .ant-notification-notice-with-icon > .ant-notification-notice-description').should('have.text','Bienvenidx a nuestra suuuper red social german!!')  
+   cy.url().should('include', '/main')
+   cy.wait(3000)
+   cy.window()
+   .its("store")
+   .invoke("getState")
+   .its("auth.user.user")
+   .should("deep.include", {
+     email: 'german@gmail.com',
+     username: 'german',
+  })
    
-   
-
-    });
 })
 
-
+})
