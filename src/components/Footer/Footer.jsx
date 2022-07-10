82d1b2007136067be3821e86dadcebdc234022f5
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-10 13:27:40 +0200

    estilos home

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,16 @@ import './Footer.scss'
 
 
 const Footer = () => {
-  return (<div className='footer'>
+  return (<><hr />
+  <div className='footer social'>
+    
     <span>Vanesa B Julio 2022 © </span>
-    <div className="social">
+    <div>
     <a href="https://www.linkedin.com/in/vanesa-b-a59b6a230/"><LinkedinOutlined className='svg' /></a>
     <a href="https://github.com/vaneebg"><GithubOutlined className='svg' /></a>
+    </div>
   </div>
-  </div>
+  </>
   )
 }
 
