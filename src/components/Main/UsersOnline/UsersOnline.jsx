3377f4443fe47ce08c1d5d27e005ebe2c24bbcd6
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-11 13:52:13 +0200

    implementado poder llevar al perfil en users conectados

--- src/components/Main/UsersOnline/UsersOnline.jsx
+++ src/components/Main/UsersOnline/UsersOnline.jsx
@@ -1,6 +1,10 @@
 import { useDispatch,useSelector} from "react-redux";
 import { useEffect } from 'react';
 import { allConnects } from "../../../features/auth/authSlice";
+import { Link } from 'react-router-dom'
+import { Tooltip } from 'antd';
+
+
 import './UsersOnline.scss'
 
 const URL = process.env.REACT_APP_URL
@@ -22,7 +26,11 @@ const userOnline=usersOnline?.map((el,i)=>{
     if(el._id!==userLocal.user._id){
 
             return(<>
-                   {el.image ? <img className='imgUserOnline animate__animated animate__bounceInUp animate__delay-4s'src={URL+"/images/users/" + el.image} alt=''/> : null}
+                 {el.image ? 
+                 
+                 <Tooltip placement="bottom" color="purple" title={el.username}>
+ <Link to={"/user/" + el._id}>  <img className='imgUserOnline animate__animated animate__bounceInUp animate__delay-4s'src={URL+"/images/users/" + el.image} alt=''/> </Link>               </Tooltip>
+                : null}
                    </>
               )}})
             
