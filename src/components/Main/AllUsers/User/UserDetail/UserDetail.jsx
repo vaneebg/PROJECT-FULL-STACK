9d566ebd13d90446b4211fe801e205628effe5a3
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-11 16:56:22 +0200

    perfil entrar a otro usuario estilos

--- src/components/Main/AllUsers/User/UserDetail/UserDetail.jsx
+++ src/components/Main/AllUsers/User/UserDetail/UserDetail.jsx
@@ -31,29 +31,37 @@ const UserDetail = () => {
   return (
     <div className='profileDetail'>
         <div className="headerProfile">
-        {userProfile?.user.image ? <img className='imgUserC' src={URL+"/images/users/" + userProfile?.user?.image} alt=''/> : <img className='imgUser' src={URL+"/images/users/none.jpg"} alt=''/>}
+          <div className="empty">
 
-        <span className='bold'>{userProfile?.user.username}</span>
-        <span>Número de posts {userProfile?.Number_of_posts}</span> <br />
+          </div>
+          <div className="block">
+          <div className="img">
+        {userProfile?.user.image ? <img className='imgUserProfileB' src={URL+"/images/users/" + userProfile?.user?.image} alt=''/> : <img className='imgUser' src={URL+"/images/users/none.jpg"} alt=''/>}
+        </div>
+        <div className="contentProfileT">
+        <span className='bold profileName'>{userProfile?.user.username}</span> <br />
+        <span className="textProfileD">Número de posts {userProfile?.Number_of_posts}</span> <br />
 {userProfile?.Followers!==0 ? <Tooltip title={<Followers/>}color='purple' key='purple'> 
-        <span>Número de followers {userProfile?.Followers}</span> <br /> 
+        <span className="textProfileD">Número de followers {userProfile?.Followers}</span> <br /> 
         </Tooltip>: <Tooltip visible={false} title={<Followers/>}color='purple' key='purple'> 
-        <span>Número de followers {userProfile?.Followers}</span> <br /> 
+        <span className="textProfileD">Número de followers {userProfile?.Followers}</span> <br /> 
         </Tooltip>}
 
         {userProfile?.Following!==0 ?   <Tooltip title={<Following/>}color='blue' key='blue'> 
-        <span>Número de following {userProfile?.Following}</span> <br />
+        <span className="textProfileD">Número de following {userProfile?.Following}</span> <br />
         </Tooltip> : <Tooltip visible={false} title={<Following/>}color='blue' key='blue'> 
-        <span>Número de following {userProfile?.Following}</span> <br />
+        <span className="textProfileD">Número de following {userProfile?.Following}</span> <br />
         </Tooltip> }
-       
+       <div className="empty">
+
+       </div>
       
-
-
+</div>
         </div>
-      
+       <div className="empty"></div>
+      </div>
         <Tabs defaultActiveKey="1" centered >
-    <TabPane tab="Posts" key="1">
+    <TabPane className="black" tab="Posts" key="1">
     <PostsProfileUser/>
     </TabPane>
    
