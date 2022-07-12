92be701772afb2683486310be2e295aa228e6245
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-12 11:14:33 +0200

    estilo 404 not found

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,56 @@
 import './NotFound.scss'
 
 const NotFound = () => {
-    return (<>
-       
-    </>
+    return (<div>
+ 
+
+    <section class="wrapper">
+
+        <div class="container">
+
+            <div id="scene" class="scene" data-hover-only="false">
+
+
+                <div class="circle" data-depth="1.2"></div>
+
+                <div class="one" data-depth="0.9">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <div class="two" data-depth="0.60">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <div class="three" data-depth="0.40">
+                    <div class="content">
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                        <span class="piece"></span>
+                    </div>
+                </div>
+
+                <p class="p404" data-depth="0.50">404</p>
+                <p class="p404" data-depth="0.10">404</p>
+
+            </div>
+
+            <div class="text">
+                <article>
+                    <p>Uh oh! Tas perdio o k ase <br/>Go back my friend!</p>
+                </article>
+            </div>
+
+        </div>
+    </section>
+    </div>
     )
     }
     export default NotFound
