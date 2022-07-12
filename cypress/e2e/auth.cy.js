27167deb27f7374f29f432b6352f1614fca251ff
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-12 20:20:51 +0200

    testing limpieza

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,24 +1,19 @@
 describe('Probando ciclo de autenticación', () => {
 
   beforeEach(() => {
-
     cy.restoreLocalStorage("user")
-  
   })
 
   afterEach(() => {
-
     cy.saveLocalStorage("user")
-  
   })
-
 
   it('Comprobando que existe la página y que se muestra carrousel', () => {
     cy.visit('http://localhost:3000')
     cy.get('.carrousel').should('be.visible')
   })
 
-  it("Probando register", function () {
+  it("Testing register", function () {
     cy.get('.ant-tabs-nav-wrap > .ant-tabs-nav-list > .ant-tabs-tab > #rc-tabs-0-tab-1 > .contentHome').click()
      cy.get('input[name="username"]').click().type('german')
      cy.get('input[name="email"]').click().type('german@gmail.com')
@@ -29,18 +24,18 @@ describe('Probando ciclo de autenticación', () => {
      cy.wait(3000)
      cy.get('[type="submit"]').click()
      cy.get('body').click()
-     cy.wait(1000)
+     cy.wait(3000)
      cy.get('div > .ant-notification-notice > .ant-notification-notice-content > .ant-notification-notice-with-icon > .ant-notification-notice-description').should('have.text','Usuario registrado con éxito')  
   });
 
-
-  it("Probando login", function () {
+  it("Testing login", function () {
+    cy.request('get','http://localhost:8080/users/confirmTesting')
     cy.wait(3000)
-
     cy.contains('Login').click()
     cy.get('input[name="email"]').click().type('german@gmail.com')
     cy.get('input[name="password"]').click().type('test')
     cy.get('[type="submit"]').click()
+    cy.wait(3000)
     cy.get('div > .ant-notification-notice > .ant-notification-notice-content > .ant-notification-notice-with-icon > .ant-notification-notice-description').should('have.text', 'Bienvenidx a nuestra suuuper red social german!!')
     cy.url().should('include', '/main')
     cy.wait(3000)
@@ -52,10 +47,9 @@ describe('Probando ciclo de autenticación', () => {
         email: 'german@gmail.com',
         username: 'german'
       });
-
   });
 
-  it("Probando logout",function(){
+  it("Testing logout",function(){
     cy.get('.usernameProfile').click()
     cy.wait(3000)
     cy.url().should('include', '/profile')
@@ -66,10 +60,9 @@ describe('Probando ciclo de autenticación', () => {
       .invoke("getState")
       .its("auth.user.user")
       .should("deep.equal", null);
-
   });
 
-  it("Probando login admin y borrando user", function () {
+  it("Testing login admin y borrar user", function () {
     cy.get('input[name="email"]').click().type('admin@gmail.com')
     cy.get('input[name="password"]').click().type('test')
     cy.get('[type="submit"]').click()
@@ -77,13 +70,10 @@ describe('Probando ciclo de autenticación', () => {
     cy.get('.linkAdmin').click()
     cy.url().should('include', '/admin')
     cy.wait(3000)
-
     cy.get('.ant-btn').last().click()
     cy.get('.ant-btn-primary').last().click()
     cy.wait(3000)
     cy.contains('german').should('not.exist');
-
-
   });
-
+  
 })
